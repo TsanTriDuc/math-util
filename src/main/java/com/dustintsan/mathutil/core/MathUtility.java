8a9991d1c96d5471f212368e0f3fcbd466aa65bb
tsantriduc <tsantriduc@gmail.com>
2024-06-05 19:50:38 +0700

    update main code to see green 4ever

--- src/main/java/com/dustintsan/mathutil/core/MathUtility.java
+++ src/main/java/com/dustintsan/mathutil/core/MathUtility.java
@@ -19,7 +19,8 @@ public class MathUtility {
     //n 0...20; do 21! tràn kiểu long, ko có âm giai thừa
     //0! = 1
     public static long getFactorial(int n){
-        long product = 10; //biến con heo đất, biến nhân dồn
+        //sửa lại từ 10 thành 1 để có màu xanh trở lại
+        long product = 1; //biến con heo đất, biến nhân dồn
         
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
